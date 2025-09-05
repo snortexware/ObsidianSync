b4c53134679feffcd6e5a92c1a23490ff6007a8e
snortexware <lucasmoreiraprivado@proton.me>
2025-09-05 11:53:00 -0300

    Removido arquivos desnecessarios

--- SystemDataContracts/EventsDataObject.fs
+++
@@ -1,9 +0,0 @@
-namespace SystemDataContracts
-
-open Google.Apis.Drive.v3
-
-type EventsDataObject() =
-    member val Service : DriveService
-    member val LocalRoot : DriveService
-    member val DriveRoot : DriveService
-    
